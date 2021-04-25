f9a32c417836b406de9188141c122c234a16a0ce
Lincoln Karuhanga <lincoln.karuhanga@gmail.com>
2021-04-25 17:36:50 +0300

    unnest folders

--- server/lingo_server/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class LingoServerConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'lingo_server'
-    verbose_name = 'Lingo'
